2289bc776be8e2112a46b261bacc09e13a169390
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-10-28 18:59:16 +0800

    Used environment variables

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """This code send an SMS to user if rainy weather"""
+import os
 import requests
 from twilio.rest import Client
 
-API_KEY = "e533f7259074449702b57be13b4cdc3e"
+API_KEY = os.environ.get("API_KEY")
 PARAMETERS = {
     "lat": 1.3408630000000001,
     "lon": 103.83039182212079,
@@ -10,8 +11,10 @@ PARAMETERS = {
     "appid": API_KEY,
 }
 WILL_RAIN = False
-ACCOUNT_SID = "AC3759af84b38e8b8c8db11d9d3e01f43a"
-AUTH_TOKEN = "65d4359bc6fca2d153f79732249f8b9d"
+ACCOUNT_SID = os.environ.get("ACCOUNT_SID")
+AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
+FROM_NUM = os.environ.get("FROM_NUM")
+TO_NUM = os.environ.get("TO_NUM")
 
 response = requests.get(url="https://api.openweathermap.org/data/3.0/onecall", params=PARAMETERS)
 response.raise_for_status()
@@ -27,7 +30,7 @@ if WILL_RAIN:
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
     message = client.messages.create(
         body="It's going to rain today. Remember to bring an umbrella! ☔️",
-        from_="+19034833039",
-        to="+6582289433"
+        from_=FROM_NUM,
+        to=TO_NUM
     )
     print(message.status)
